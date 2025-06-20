c3932f6feeb42c854f64d0370b0f61c3780f8d24
Stephan Moeller <stephanryer@hotmail.com>
2025-06-20 15:54:52 +0200

    added initial code

---
+++ .zig-cache/o/198bc9732f4db1eaf3d08c1d8e629f90/cimport.h.d
@@ -0,0 +1,11 @@
+cimport.o: \
+  /home/stephan/projects/using-microzig/.zig-cache/o/198bc9732f4db1eaf3d08c1d8e629f90/cimport.h \
+  /home/stephan/.cache/zig/p/sqlite-3.47.2-AAAAACdIpQC782FmpW8DylXC9FX3DNs91fjcoffHjlfZ/c/sqlite3.h \
+  /home/stephan/programs/zig/lib/include/stdarg.h \
+  /home/stephan/programs/zig/lib/include/__stdarg_header_macro.h \
+  /home/stephan/programs/zig/lib/include/__stdarg___gnuc_va_list.h \
+  /home/stephan/programs/zig/lib/include/__stdarg_va_list.h \
+  /home/stephan/programs/zig/lib/include/__stdarg_va_arg.h \
+  /home/stephan/programs/zig/lib/include/__stdarg___va_copy.h \
+  /home/stephan/programs/zig/lib/include/__stdarg_va_copy.h \
+  /home/stephan/.cache/zig/p/sqlite-3.47.2-AAAAACdIpQC782FmpW8DylXC9FX3DNs91fjcoffHjlfZ/c/workaround.h
---
+++ README.md
@@ -0,0 +1,13 @@
+This project is just the result of following these instructions: https://microzig.tech/docs/getting-started/
+
+Steps:
+mkdir using-microzig
+cd using-microzig
+zig init
+zig fetch --save https://github.com/ZigEmbeddedGroup/microzig/releases/download/0.14.1/microzig.tar.gz
+
+
+Now we can build the thing and copy the .uf2 file to the mcu:
+
+zig build
+cd ./zig-out/firmware
